03b341067229762e58d780ef1cc83efe308b170b
程昊 <shenlinxian@hotmail.com>
2024-12-17 16:09:00 +0800

    bash

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 if [ "$1" -eq 1 ]; then
     cd ./experiment/honeybadger_test/ && ./honeybadger_test --id=$2
 else
-    cd ./experiment/fin/ && ./fin --id=$2
+    cd ./experiment/fin_test/ && ./fin_test --id=$2
 fi
